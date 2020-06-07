2ebf1a8669b5562a64de5758bf9fde7afb294277
Arie May Wibowo <arie.wibowo@cimbniaga.co.id>
2020-06-08 00:43:43 +0700

    learn basic spritekit

--- SpriteKitTutorial/GameViewController.swift
+++ SpriteKitTutorial/GameViewController.swift
@@ -1,50 +1,12 @@
-//
-//  GameViewController.swift
-//  SpriteKitTutorial
-//
-//  Created by Arie May Wibowo on 08/06/20.
-//  Copyright © 2020 Arie May Wibowo. All rights reserved.
-//
 
-import UIKit
+
 import SpriteKit
-import GameplayKit
 
 class GameViewController: UIViewController {
-
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        if let view = self.view as! SKView? {
-            // Load the SKScene from 'GameScene.sks'
-            if let scene = SKScene(fileNamed: "GameScene") {
-                // Set the scale mode to scale to fit the window
-                scene.scaleMode = .aspectFill
-                
-                // Present the scene
-                view.presentScene(scene)
-            }
-            
-            view.ignoresSiblingOrder = true
-            
-            view.showsFPS = true
-            view.showsNodeCount = true
-        }
-    }
-
-    override var shouldAutorotate: Bool {
-        return true
-    }
-
-    override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
-        if UIDevice.current.userInterfaceIdiom == .phone {
-            return .allButUpsideDown
-        } else {
-            return .all
-        }
-    }
-
-    override var prefersStatusBarHidden: Bool {
-        return true
+        let scene = GameScene(size: view.frame.size)
+        let skView = view as! SKView
+        skView.presentScene(scene)
     }
 }
